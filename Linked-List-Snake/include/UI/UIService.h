5d48cac13c4d870b4017df60fb038da96bd4d61e
Dhiraj Magdum <dhiraj.magdum@outscal.com>
2023-11-08 17:36:02 +0530

    Added namespace for classes

---
+++ Linked-List-Snake/include/UI/UIService.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "UI/MainMenu/MainMenuUIController.h"
+#include "UI/SplashScreen/SplashScreenUIController.h"
+#include "UI/Interface/IUIController.h"
+
+namespace UI
+{
+	class UIService
+	{
+	private:
+		SplashScreen::SplashScreenUIController* splash_screen_controller;
+		MainMenu::MainMenuUIController* main_menu_controller;
+
+		void createControllers();
+		void initializeControllers();
+		Interface::IUIController* getCurrentUIController();
+		void destroy();
+
+	public:
+		UIService();
+		~UIService();
+
+		void initialize();
+		void update();
+		void render();
+		void showScreen();
+	};
+}
---
+++ Linked-List-Snake/source/Global/Config.cpp
@@ -0,0 +1,22 @@
+#include "Global/Config.h"
+
+namespace Global
+{
+	const sf::String Config::outscal_logo_texture_path = "assets/textures/outscal_logo.png";
+
+	const sf::String Config::background_texture_path = "assets/textures/snake_bg.png";
+
+	const sf::String Config::play_button_texture_path = "assets/textures/play_button.png";
+
+	const sf::String Config::instructions_button_texture_path = "assets/textures/instructions_button.png";
+
+	const sf::String Config::quit_button_texture_path = "assets/textures/quit_button.png";
+
+	const sf::String Config::bubble_bobble_font_path = "assets/fonts/bubbleBobble.ttf";
+
+	const sf::String Config::DS_DIGIB_font_path = "assets/fonts/DS_DIGIB.ttf";
+
+	const sf::String Config::background_music_path = "assets/sounds/background_music.mp3";
+
+	const sf::String Config::button_click_sound_path = "assets/sounds/button_click_sound.wav";
+}
